29bde9f30e367e887551a7979d5b53bad64ba94b
Patryk Dudizński <dodpat02@gmail.com>
2025-11-20 00:12:40 +0100

    update sub

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 18
-#define VERSION_BUILD 1
+#define VERSION_MINOR 21
+#define VERSION_BUILD 3
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:0.18.1"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:0.21.3"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
